7088dec4f4274eaeed5dea2f479babf74a33e25f
greglobinski <greglobinski@gmail.com>
2018-07-25 00:01:14 +0200

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <Layout>
+    <Article>
+      <Heading title="NOT FOUND" />
+      <Bodytext>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Bodytext>
+    </Article>
+  </Layout>
+);
+
+export default NotFoundPage;
