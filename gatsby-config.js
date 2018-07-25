f5ef42dc5b729630d78743206d8a32f45fc1f751
greglobinski <greglobinski@gmail.com>
2018-07-25 14:35:32 +0200

    fix: remove gatsby-plugin-sitemap from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,19 @@ module.exports = {
   siteMetadata: {
     title: config.siteTitle,
     description: config.siteDescription,
-    siteUrl: config.siteUrl
+    siteUrl: config.siteUrl,
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `parts`,
-        path: `${__dirname}/content/parts/`
-      }
+        path: `${__dirname}/content/parts/`,
+      },
     },
     {
-      resolve: `gatsby-transformer-remark`
+      resolve: `gatsby-transformer-remark`,
     },
     `gatsby-plugin-catch-links`,
-    `gatsby-plugin-sitemap`
-  ]
+  ],
 };
