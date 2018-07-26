76d3186d4b94775c5055a66614809999f51e7145
greglobinski <greglobinski@gmail.com>
2018-07-26 11:49:53 +0200

    Remove SEO

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,21 +6,13 @@ import config from '../../../content/meta/config';
 import menu from '../../../content/meta/menu';
 import Footer from '../Footer';
 import Header from '../Header';
-import Seo from '../Seo';
 
 import '../../styles/graph.css';
 
 const Layout = props => {
-  const { children, location, seoTitle = '', seoDescription = '' } = props;
+  const { children, location } = props;
 
-  const {
-    headerTitle,
-    headerSubTitle,
-    siteTitle,
-    siteDescription,
-    siteLanguage,
-    siteUrl,
-  } = config;
+  const { headerTitle, headerSubTitle } = config;
 
   return (
     <StaticQuery
@@ -41,12 +33,6 @@ const Layout = props => {
             <Header title={headerTitle} subTitle={headerSubTitle} menu={menu} />
             <main>{children}</main>
             <Footer footnote={footnote} />
-            <Seo
-              language={siteLanguage}
-              title={seoTitle ? seoTitle : siteTitle}
-              description={seoDescription ? seoDescription : siteDescription}
-              url={siteUrl}
-            />
           </div>
         );
       }}
